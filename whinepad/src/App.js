24bfea4e224b2e102f8f4d2c2365e2569f072327
Michael <mikhail6886@yandex.ru>
2024-06-12 00:59:11 +0500

    added the new components

--- whinepad/src/App.js
+++ whinepad/src/App.js
@@ -1,31 +1,25 @@
-// import logo from './logo.svg';
 import './App.css';
-import Excel from './components/Excel';
 import Discovery from './components/Discovery';
-
-const headers = localStorage.getItem('headers');
-const data = localStorage.getItem('data');
-
-if(!headers) {
-  // headers = ['Title', 'Year', 'Raiting', 'Comments'];
-  // data = ['Red whine', '2021', '3', 'meh'];
-}
+import DataFlow from './components/DataFlow';
+// uncomment for the state-only option
+// import DataFlow from './components/DataFlow1';
+import schema from './config/schema';
 
 const isDiscovery = window.location.pathname.replace(/\//g, '') === 'discovery';
 
-function App() {
+let data = JSON.parse(localStorage.getItem('data'));
 
+// default example data, read from the schema
+if (!data) {
+  data = [{}];
+  Object.keys(schema).forEach((key) => (data[0][key] = schema[key].samples[0]));
+}
+
+function App() {
   if (isDiscovery) {
     return <Discovery />;
   }
-  return (
-    <div>
-      <Excel 
-        headers={headers} 
-        initialData={data}
-      />
-    </div>
-  );
+  return <DataFlow schema={schema} initialData={data} />;
 }
 
 export default App;
